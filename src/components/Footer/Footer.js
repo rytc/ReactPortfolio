b3f1fcbbefd778bb476c4b85a2721c3b414ccf73
rytc <rytc@protonmail.com>
2022-01-14 17:35:18 -0800

    Revert gh-pages push

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,12 @@
+import GithubIcon from "../../images/github.png"
+import LinkedInIcon from "../../images/linkedin.png"
 
 const Footer = (props) => {
     return (
         <footer className="container mt-5 border-top">
             <p className="text-secondary">&copy; 2022 Ryan Capote</p>
+            <a href="https://github.com/rytc"><img src={GithubIcon}/></a>
+            <a href="https://www.linkedin.com/in/ryan-capote-52a83622/"><img src={LinkedInIcon}/></a>
         </footer>
     )
 }
